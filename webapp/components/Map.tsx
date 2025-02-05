9ff39df0d6c7f25f13ee26111c9731b60924f381
Hsun-Pei Wang <garfieldduck@gmail.com>
2025-02-05 16:02:35 +0100

    🎨 Format the code

--- webapp/components/Map.tsx
+++ webapp/components/Map.tsx
@@ -2,19 +2,19 @@
 
 import { useEffect } from "react";
 
-import ReactMapGl from 'react-map-gl/maplibre';
+import ReactMapGl from "react-map-gl/maplibre";
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 import { Protocol } from "pmtiles";
-import layers from 'protomaps-themes-base';
-import { Leva, useControls } from 'leva'
+import layers from "protomaps-themes-base";
+import { Leva, useControls } from "leva";
 
 const SOURCE = "protomaps";
 
 export default function Map() {
   // controls from Leva is a library for adding a GUI to help us try out different styles.
   // we're going to discard it once designers make a decision on the map style
-  const { 
+  const {
     theme,
     language,
     customizeCountryBorders,
@@ -24,45 +24,44 @@ export default function Map() {
     regionBorderWidth,
     regionBorderColor,
     regionDashline,
-  } = useControls(
-     {
-      theme:{
-        options: ['light', 'dark', 'white', 'grayscale', 'black'],
-        value: 'white',
-      },
-      language:{
-        options: ['en', 'fr'],
-        value: 'fr',
-      },
-      // country
-      customizeCountryBorders: {
-        value: true,
-      },
-      countryBorderWidth: {
-        value: 3,
-        min: 1,
-        max: 10,
-        step: 1,
-        render: (get) => get('customizeCountryBorders')
-      },
-      countryBorderColor: "#bdb8b8",
-      // region
-      customizeRegionBorders: {
-        value: true,
-      },
-      regionBorderWidth: {
-        value: 2,
-        min: 1,
-        max: 8,
-        step: 1,
-        render: (get) => get('customizeRegionBorders')
-      },
-      regionBorderColor: "#d7d7d7",
-      regionDashline: {
-        value: true,
-        render: (get) => get('customizeRegionBorders')
-      }
-    });
+  } = useControls({
+    theme: {
+      options: ["light", "dark", "white", "grayscale", "black"],
+      value: "white",
+    },
+    language: {
+      options: ["en", "fr"],
+      value: "fr",
+    },
+    // country
+    customizeCountryBorders: {
+      value: true,
+    },
+    countryBorderWidth: {
+      value: 3,
+      min: 1,
+      max: 10,
+      step: 1,
+      render: (get) => get("customizeCountryBorders"),
+    },
+    countryBorderColor: "#bdb8b8",
+    // region
+    customizeRegionBorders: {
+      value: true,
+    },
+    regionBorderWidth: {
+      value: 2,
+      min: 1,
+      max: 8,
+      step: 1,
+      render: (get) => get("customizeRegionBorders"),
+    },
+    regionBorderColor: "#d7d7d7",
+    regionDashline: {
+      value: true,
+      render: (get) => get("customizeRegionBorders"),
+    },
+  });
 
   useEffect(() => {
     const protocol = new Protocol();
@@ -72,69 +71,78 @@ export default function Map() {
     };
   }, []);
 
-  console.log('layers(SOURCE, "white", "en")', layers(SOURCE, "white", "en"))
+  console.log('layers(SOURCE, "white", "en")', layers(SOURCE, "white", "en"));
 
   return (
     <>
       <ReactMapGl
-        style={{width: "100%", height: "90vh"}}
+        style={{ width: "100%", height: "90vh" }}
         mapStyle={{
           version: 8,
-          glyphs: "https://protomaps.github.io/basemaps-assets/fonts/{fontstack}/{range}.pbf",
-          sprite: "https://protomaps.github.io/basemaps-assets/sprites/v4/light",
+          glyphs:
+            "https://protomaps.github.io/basemaps-assets/fonts/{fontstack}/{range}.pbf",
+          sprite:
+            "https://protomaps.github.io/basemaps-assets/sprites/v4/light",
           sources: {
             protomaps: {
               type: "vector",
-              "maxzoom": 15,
+              maxzoom: 15,
               url: "https://api.protomaps.com/tiles/v4.json?key=707d8bc70b393fc0",
-              "attribution": "<a href=\"https://osm.org/copyright\">© OpenStreetMap</a>",
+              attribution:
+                '<a href="https://osm.org/copyright">© OpenStreetMap</a>',
             },
           },
           layers: [
             ...layers(SOURCE, theme, language).filter(
-              (layer) => ![
-                customizeCountryBorders ? "boundaries_country" : undefined,
-                customizeRegionBorders ? "places_region" : undefined,
-              ].includes(layer.id)
+              (layer) =>
+                ![
+                  customizeCountryBorders ? "boundaries_country" : undefined,
+                  customizeRegionBorders ? "places_region" : undefined,
+                ].includes(layer.id),
             ),
-            ...(customizeCountryBorders ? [{
-                "id": "boundaries_country",
-                "type": "line",
-                "source": SOURCE,
-                "source-layer": "boundaries",
-                "filter": ['<=', 'kind_detail', 2],
-                paint: {
-                  "line-color": countryBorderColor,
-                  "line-width": countryBorderWidth,
-                }
-            } satisfies maplibregl.LayerSpecification] : []),
-            ...(customizeRegionBorders ? [{
-              "id": "places_region",
-              "type": "line",
-              "source": SOURCE,
-              "source-layer": "boundaries",
-              "filter": ['==', 'kind', 'region'],
-              paint: {
-                "line-color": regionBorderColor,
-                "line-width": regionBorderWidth,
-                "line-dasharray": regionDashline ? [2, 3] : undefined,
-              }
-            } satisfies maplibregl.LayerSpecification] : []),
-          ]
-      }}
+            ...(customizeCountryBorders
+              ? [
+                  {
+                    id: "boundaries_country",
+                    type: "line",
+                    source: SOURCE,
+                    "source-layer": "boundaries",
+                    filter: ["<=", "kind_detail", 2],
+                    paint: {
+                      "line-color": countryBorderColor,
+                      "line-width": countryBorderWidth,
+                    },
+                  } satisfies maplibregl.LayerSpecification,
+                ]
+              : []),
+            ...(customizeRegionBorders
+              ? [
+                  {
+                    id: "places_region",
+                    type: "line",
+                    source: SOURCE,
+                    "source-layer": "boundaries",
+                    filter: ["==", "kind", "region"],
+                    paint: {
+                      "line-color": regionBorderColor,
+                      "line-width": regionBorderWidth,
+                      "line-dasharray": regionDashline ? [2, 3] : undefined,
+                    },
+                  } satisfies maplibregl.LayerSpecification,
+                ]
+              : []),
+          ],
+        }}
         initialViewState={{
           longitude: 2.213749,
           latitude: 46.227638,
-          zoom: 6
+          zoom: 6,
         }}
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         mapLib={maplibregl as any}
       />
       {/* TODO: remove this once the design decision is made */}
-      <Leva
-        oneLineLabels
-      />
+      <Leva oneLineLabels />
     </>
-      
   );
 }
