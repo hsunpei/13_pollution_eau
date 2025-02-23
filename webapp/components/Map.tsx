9cb1f28ffa61abb2bddc5274df3ccc4a1eb783a3
Hsun-Pei Wang <garfieldduck@gmail.com>
2025-02-23 21:45:22 +0100

    🔥 Remove unncesseary Fragment

--- webapp/components/Map.tsx
+++ webapp/components/Map.tsx
@@ -22,46 +22,43 @@ export default function Map() {
   }, []);
 
   return (
-    <>
-      <ReactMapGl
-        style={{ width: "100%", height: "90vh" }}
-        mapStyle={{
-          version: 8,
-          glyphs:
-            "https://protomaps.github.io/basemaps-assets/fonts/{fontstack}/{range}.pbf",
-          sprite:
-            "https://protomaps.github.io/basemaps-assets/sprites/v4/light",
-          sources: {
-            protomaps: {
-              type: "vector",
-              maxzoom: MAPLIBRE_MAP.protomaps.maxzoom,
-              url: `https://api.protomaps.com/tiles/v4.json?key=${MAPLIBRE_MAP.protomaps.api_key}`,
-              attribution:
-                '<a href="https://osm.org/copyright">© OpenStreetMap</a>',
-            },
+    <ReactMapGl
+      style={{ width: "100%", height: "90vh" }}
+      mapStyle={{
+        version: 8,
+        glyphs:
+          "https://protomaps.github.io/basemaps-assets/fonts/{fontstack}/{range}.pbf",
+        sprite: "https://protomaps.github.io/basemaps-assets/sprites/v4/light",
+        sources: {
+          protomaps: {
+            type: "vector",
+            maxzoom: MAPLIBRE_MAP.protomaps.maxzoom,
+            url: `https://api.protomaps.com/tiles/v4.json?key=${MAPLIBRE_MAP.protomaps.api_key}`,
+            attribution:
+              '<a href="https://osm.org/copyright">© OpenStreetMap</a>',
           },
-          layers: [
-            ...layers(
-              SOURCE,
-              MAPLIBRE_MAP.protomaps.theme,
-              MAPLIBRE_MAP.protomaps.language,
-            ).filter((layer) => !["boundaries_country"].includes(layer.id)),
-            {
-              id: "boundaries_country",
-              type: "line",
-              source: SOURCE,
-              "source-layer": "boundaries",
-              filter: ["<=", "kind_detail", 2],
-              paint: {
-                "line-color": MAPLIBRE_MAP.countryBorderColor,
-                "line-width": MAPLIBRE_MAP.countryBorderWidth,
-              },
-            } satisfies maplibregl.LayerSpecification,
-          ],
-        }}
-        initialViewState={MAPLIBRE_MAP.initialViewState}
-        mapLib={maplibregl}
-      />
-    </>
+        },
+        layers: [
+          ...layers(
+            SOURCE,
+            MAPLIBRE_MAP.protomaps.theme,
+            MAPLIBRE_MAP.protomaps.language,
+          ).filter((layer) => !["boundaries_country"].includes(layer.id)),
+          {
+            id: "boundaries_country",
+            type: "line",
+            source: SOURCE,
+            "source-layer": "boundaries",
+            filter: ["<=", "kind_detail", 2],
+            paint: {
+              "line-color": MAPLIBRE_MAP.countryBorderColor,
+              "line-width": MAPLIBRE_MAP.countryBorderWidth,
+            },
+          } satisfies maplibregl.LayerSpecification,
+        ],
+      }}
+      initialViewState={MAPLIBRE_MAP.initialViewState}
+      mapLib={maplibregl}
+    />
   );
 }
